526a46060542d14d44d8aa943e201ac2006f7ce5
Nii-sanHaxor <45889833+arifarhan16@users.noreply.github.com>
2019-06-21 22:06:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
-ini inidez
+<html>
+
+    <head>
+        <meta content='Hacked by NiiX_' property='og:title' />
+        <meta content='article' property='og:type' />
+        <title>Hacked by NiiX_</title>
+    <script src="https://cdn.rawgit.com/FicriPebriyana/efek/0a935a6c/efek%20salju.js" type="text/javascript"></script>
+
+    <body bgcolor="black">
+        <style>
+            h1 {
+                text-align: center;
+                color: #3335cf;
+                font-size: 35px;
+                font-family: Arial Narrow, sans-serif;
+                font-style: garamond;
+                text-shadow: 100vmax #f00505;
+                >text-shadow: 0px 0px 0px #f00505; }
+                >
+        </style>
+        <br><br><br><br><br><br><br>
+        <script>
+window.alert('Keep Down And SLow My Admin:)');
+
+            </script>
+
+
+<iframe width="0%" height="0" scrolling="no" frameborder="no" loop="true" allow="autoplay"
+    src="https://h.top4top.net/m_1267gu55s1.mp3"></iframe>
+            
+        <center>
+            <img src="https://sociorocketnewsen.files.wordpress.com/2014/12/ezgif-1923448175.gif?w=580&h=580&resize=580%2C580"
+                height="330px" width="330px">
+        </center>
+        <center>
+            <font color=red size=3 face=courier new>Indon<font color=white>esian People</font>
+        </center>
+        <center>
+            <font color=#077f01 size=5 face=courier new>Touched by NiiX_</font>
+            <center>
+                <center>
+                    <font color=#c6c5c4 size=2 face=courier new>[ <font color=#a3a2a1>[ Friendshit ]<font
+                                color=#c6c5c4> ]</font>
+                </center>
